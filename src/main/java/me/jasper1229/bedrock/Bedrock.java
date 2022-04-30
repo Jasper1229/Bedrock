ae6c6c7836d34604f49ca59a691e37a14535be84
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 03:22:44 -0500

    added teleport command

--- src/main/java/me/jasper1229/bedrock/Bedrock.java
+++ src/main/java/me/jasper1229/bedrock/Bedrock.java
@@ -4,14 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bedrock extends JavaPlugin {
 
+    private static Bedrock instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        saveDefaultConfig();
+        getLogger().info("Bedrock has been enabled! ♥");
+        getCommand("btp").setExecutor(new TeleportCommand(this));
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Bedrock has been disabled! See you next time!");
     }
+
 }
---
+++ src/main/java/me/jasper1229/bedrock/TeleportCommand.java
@@ -0,0 +1,40 @@
+package me.jasper1229.bedrock;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class TeleportCommand implements CommandExecutor {
+
+    private final Bedrock plugin;
+
+    public TeleportCommand(Bedrock plugin) {
+        this.plugin = plugin;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if (sender instanceof Player)
+        {
+            Player playerSender = (Player) sender;
+
+            if (!args[1].contains(Bukkit.getOnlinePlayers().toString()) || args.length > 1)
+            {
+                playerSender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("plugin-prefix")) + ChatColor.LIGHT_PURPLE + " /btp <Online Player>");
+                return true;
+            }
+
+            Player playerRequested = Bukkit.getPlayer(args[0]);
+            playerSender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("plugin-prefix")) + ChatColor.LIGHT_PURPLE + " Teleporting");
+            playerSender.teleport(playerRequested.getLocation());
+            return true;
+        }
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("plugin-prefix")) + "Can only be run be players!");
+        return false;
+    }
+
+}
