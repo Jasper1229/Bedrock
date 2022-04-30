1e59ac386983ff3ecb155794e54f00a58b46038a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 02:35:48 -0500

    Project folders and files :)

---
+++ src/main/java/me/jasper1229/bedrock/Bedrock.java
@@ -0,0 +1,17 @@
+package me.jasper1229.bedrock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bedrock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
