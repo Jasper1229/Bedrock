b94f782fddc843f893c5609dfff73cc4026b4c98
Blurmit <87097184+Blurmit@users.noreply.github.com>
2022-08-10 10:57:23 -0500

    Kick Update

---
+++ src/main/java/me/jasper1229/bedrock/commands/punish/Kick.java
@@ -0,0 +1,29 @@
+package me.jasper1229.bedrock.commands.punish;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Kick implements CommandExecutor
+{
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 2) {
+            sender.sendMessage(ChatColor.RED + "Invalid arguments! Usage: /kick <player> <reason>");
+            return true;
+        }
+        
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            sender.sendMessage(ChatColor.RED + "Error: Player not found.");
+            return true;
+        }
+
+        String reason = String.join(" ", args, 1);
+        target.disconnect(ChatColor.translateAlternateColorCodes('&', reason));
+
+        sender.sendMessage(ChatColor.GREEN + "Sucessfully kicked " + target.getName() + " from server.");
+        return true;
+    }
+}
